895b2cbdf8c28a1de86b5f7cf89bfc295a7f7954
Grant <grant@localhost.localdomain>
2016-04-08 19:00:47 -0400

    Comment on what keeper is

--- keeper/keeper.go
+++ keeper/keeper.go
@@ -4,8 +4,16 @@ import (
   "fmt"
 )
 
+/*******************************************************************************
+  Keeper is a microservice that will run on each block in blockchain that will
+  access the data stored on the node and serve requests from the microservices
+  in the local node.
+*******************************************************************************/
+
 func main() {
-  originalString := "BWAHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
+
+  //Testing encrypt files
+  originalString := "THIS IS A TEST ARGHHHHh!"
   fmt.Println(originalString)
 
   encryptedString := Encrypt(originalString, "1234567891234567")

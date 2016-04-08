316c99e135768d3886bc1097cf3854908b04634f
Grant <grant@localhost.localdomain>
2016-04-08 01:51:49 -0400

    Set up instruction structs and functions for handling JSON

---
+++ instructions.go
@@ -0,0 +1,46 @@
+package jin
+
+import (
+  "encoding/json"
+)
+type Payload struct {
+  Instructions Instruction
+}
+
+type Instruction struct {
+  Url             Urls
+  InstructionType InstructionTypes
+  Specification   Specifications
+}
+
+type Urls map[string]string
+type InstructionTypes map[string]string
+type Specifications map[string]string
+
+func WriteInst(SourceIp, TargetIp, InstructionType, Specification string) ([]byte, error) {
+
+  Ips := make(map[string]string)
+  Ips["Source"] = SourceIp
+  Ips["Target"] = TargetIp
+
+  InsType := make(map[string]string)
+  InsType["InstructionType"] = InstructionType
+
+  Spec := make(map[string]string)
+  Spec["Specification"] = Specification
+
+  i := Instruction{Ips, InsType, Spec}
+  p := Payload{i}
+  return json.Marshal(p)
+}
+
+func ReadInstruction(slice []byte) (Payload) {
+  var payload Payload
+  err := json.Unmarshal(slice, &payload)
+
+  if err != nil {
+    panic(err)
+  }
+
+  return payload
+}

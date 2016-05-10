ad21e43b8de08dfd1686ad64415203bdf955362c
Grant <grantseltzer@gmail.com>
2016-05-10 18:02:39 -0400

    Fixed package name, need to move to keeper

--- src/whisper/postTasks.go
+++ src/whisper/postTasks.go
@@ -1,36 +1,36 @@
-package main
+package whisper
 
-import (
-  "net/http"
-  "fmt"
-  "encoding/json"
-  "bytes"
-  "common"
-)
-
-// MOVE TO KEEPER, the post request that this function returns can be added to a queue for whisper to pick up off of to execute
-func postIP(ip string, targetIp string) (*http.Request) {
-    newIp := &common.Ip{ip}
-    jsonNewIp, jsonError := json.Marshal(newIp)
-    if (jsonError != nil) {
-        panic(jsonError)
-    }
-    postRequest, postError := http.NewRequest("POST", targetIp, bytes.NewBuffer(jsonNewIp))
-    if (postError != nil) {
-        panic(postError)
-    }
-    return postRequest
-}
-
-
-// ^ USAGE:
-func main() {
-    ourRequest := postIP("grantisAwesome", "http://localhost:8481/newIp")
-    fmt.Println(ourRequest)
-    client := &http.Client{}
-    resp, err := client.Do(ourRequest)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("Response: ", resp)
-}
+// import (
+//   "net/http"
+//   "fmt"
+//   "encoding/json"
+//   "bytes"
+//   "common"
+// )
+//
+// // MOVE TO KEEPER, the post request that this function returns can be added to a queue for whisper to pick up off of to execute
+// func postIP(ip string, targetIp string) (*http.Request) {
+//     newIp := &common.Ip{ip}
+//     jsonNewIp, jsonError := json.Marshal(newIp)
+//     if (jsonError != nil) {
+//         panic(jsonError)
+//     }
+//     postRequest, postError := http.NewRequest("POST", targetIp, bytes.NewBuffer(jsonNewIp))
+//     if (postError != nil) {
+//         panic(postError)
+//     }
+//     return postRequest
+// }
+//
+//
+// // // ^ USAGE:
+// // func main() {
+// //     ourRequest := postIP("grantisAwesome", "http://localhost:8481/newIp")
+// //     fmt.Println(ourRequest)
+// //     client := &http.Client{}
+// //     resp, err := client.Do(ourRequest)
+// //     if err != nil {
+// //         panic(err)
+// //     }
+// //     fmt.Println("Response: ", resp)
+// // }

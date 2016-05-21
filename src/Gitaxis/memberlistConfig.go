a9c8a9479450a40c6bfb9b3cde58093fc82619b6
Grant <grantseltzer@gmail.com>
2016-05-20 23:36:59 -0400

    Documentation

--- src/Gitaxis/memberlistConfig.go
+++ src/Gitaxis/memberlistConfig.go
@@ -4,7 +4,13 @@ import (
   "fmt"
   "github.com/hashicorp/memberlist"
 )
+/*******************************************************************************
+  memberlist.config will be a formatted file that stores information about the
+  botnet instances memberlist (a library provided by great folks at Hashicorp)
 
+  The purpose of this file is to maintain a persistent record of previously
+  known ips on the botnet incase a file
+*******************************************************************************/
 func newConfig() {
-  
+
 }
